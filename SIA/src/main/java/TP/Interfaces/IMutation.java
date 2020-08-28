c549cb958a693323ce09560d6938d7e5da5d4781
Lautaro Pinilla <lpinilla@itba.edu.ar>
2020-08-28 16:44:41 -0300

    Hecho comentario y descripta función de mutación

--- SIA/src/main/java/TP/Interfaces/IMutation.java
+++ SIA/src/main/java/TP/Interfaces/IMutation.java
@@ -1,4 +1,14 @@
 package TP.Interfaces;
 
+import TP.Models.Genetics.Chromosome;
+
 public interface IMutation {
+
+    /**
+     * Método que se aplica sobre un cromosoma para
+     * @param c cromosoma a mutar
+     * @return void
+     */
+    public void mutate(Chromosome c);
+
 }
