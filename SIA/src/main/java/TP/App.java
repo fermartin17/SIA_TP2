ef301ce9d2e781bdd6d24e1754f98210a17a56f4
fermartin17 <femartin@itba.edu.ar>
2020-09-02 15:45:30 -0300

    configuration

--- SIA/src/main/java/TP/App.java
+++ SIA/src/main/java/TP/App.java
@@ -2,16 +2,15 @@ package TP;
 
 import TP.Helpers.CSVImportHelper;
 import TP.Interfaces.ICSVImportHelper;
+import TP.Interfaces.ISelection;
+import TP.Models.BasePlayer;
+import TP.Models.Genetics.Selections.EliteSelection;
+import TP.Models.Genetics.Selections.RouletteSelection;
+
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
-
-        ICSVImportHelper importer = new CSVImportHelper();
-
-        importer.setData();
-
-        System.out.println("finished");
     }
 }
---
+++ SIA/src/main/java/TP/Models/ConfigurationFile.java
@@ -0,0 +1,13 @@
+package TP.Models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ConfigurationFile {
+//    private String Crossover;
+//    private String Mutation;
+//    private String Selection;
+//    private String
+}
